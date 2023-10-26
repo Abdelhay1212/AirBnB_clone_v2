e908ecf5c96dc66f4fcd1c8b55fee0d3d0767ea9
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-26 16:45:56 +0200

    updated

--- models/state.py
+++ models/state.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""defines the State class."""
+""" holds class State"""
 import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """represents a state for a MySQL database.
-    """
+    """Representation of state """
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
@@ -20,12 +16,17 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """get a list of all related City objects."""
+            """getter for list of city instances related to the state"""
             city_list = []
-            for city in list(models.storage.all(City).values()):
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
