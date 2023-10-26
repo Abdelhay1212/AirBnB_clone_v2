f42615c39e538a900df7def61d7b2649d01f76a6
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-26 15:18:15 +0200

    updated

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -36,5 +36,11 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    status = "even" if n % 2 == 0 else "odd"
+    return render_template("6-number_odd_or_even.html", n=n, status=status)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
