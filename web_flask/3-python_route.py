e164be0e067c1ed889d645a4344a2f6b47bde0b4
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-26 14:24:12 +0200

    updated

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,8 +21,9 @@ def C(text):
     return f"C {text}"
 
 
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
+def python(text):
     text = text.replace('_', ' ')
     return f"Python {text}"
 
